6d0212f08b8edbbcdb0a7f14a92cad1f0dbb9d4e
kjell_swedin <devnull@localhost>
2011-09-22 16:53:14 -0700

    additional activity scenarios running. Result not yet as expected

--- test/test_driver.py
+++ test/test_driver.py
@@ -36,30 +36,48 @@ def run_tests(consumer, fuelbed_list, outfile):
     results = consumer.results()
     write_csv(results['consumption'], fuelbed_list, outfile)
 
-activityZero = {
-    'fuel_moisture_10hr_pct':15,
-    'fuel_moisture_1000hr_pct':39,
-    'fuelbed_area_acres':10,
-    'lengthOfIgnition':5
-    }
+def run_additional_activity_scenarios(consumer, fuelbed_list):
+    activityTwo = {
+        'fuel_moisture_10hr_pct':15,
+        'fuel_moisture_1000hr_pct':39,
+        'fuelbed_area_acres':10,
+        'lengthOfIgnition':5
+        }
 
-activityTwo = {
-    'fuel_moisture_10hr_pct':15,
-    'fuel_moisture_1000hr_pct':45,
-    'fuelbed_area_acres':25,
-    }
+    activityThree = {
+        'fuel_moisture_10hr_pct':15,
+        'fuel_moisture_1000hr_pct':45,
+        'fuelbed_area_acres':25,
+        }
 
-activityThree = {
-    'fuel_moisture_10hr_pct':17,
-    'fuel_moisture_1000hr_pct':50,
-    'fuelbed_area_acres':100,
-    }
+    activityFour = {
+        'fuel_moisture_10hr_pct':17,
+        'fuel_moisture_1000hr_pct':50,
+        'fuelbed_area_acres':100,
+        }
 
-activityFour = {
-    'fuel_moisture_10hr_pct':25,
-    'fuel_moisture_1000hr_pct':55,
-    'fuelbed_area_acres':100,
-    }
+    activityFive = {
+        'fuel_moisture_10hr_pct':25,
+        'fuel_moisture_1000hr_pct':55,
+        'fuelbed_area_acres':100,
+        }
+
+    scenario_list = ['activityTwo', 'activityThree', 'activityFour', 'activityFive']
+    counter = 2
+    for scene in scenario_list:
+        SetDefaults(consumer, scene)
+        consumer.fuelbed_ecoregion = 'western'
+        consumer.burn_type = 'activity'
+        outfilename = "activity{}_out.csv".format(counter)
+        counter += 1
+        with open(outfilename, 'w') as outfile:
+            run_tests(consumer, fuelbed_list, outfile)
+        reference_values = "activity{}_expected.csv".format(counter)
+        ref = compareCSV(reference_values, console=False)
+        computed = compareCSV(outfilename, console=False)
+        (failed, compared) = ref.Compare(computed)
+        print("{}: failed = {} compared = {}".format(outfilename, failed, compared))
+
 
 def SetDefaults(consumer, map):
     consumer.burn_type = map['burn_type'] if 'burn_type' in map else 'natural'
@@ -77,6 +95,20 @@ def SetDefaults(consumer, map):
     consumer.slope = map['slope'] if 'slope' in map else 5
     consumer.windspeed = map['windspeed'] if 'windspeed' in map else 5
 
+def run_basic_scenarios(consumer, fuelbed_list):
+    scenario_list = ['western', 'southern', 'boreal', 'activity']
+    for scene in scenario_list:
+        consumer.fuelbed_ecoregion = scene if scene != 'activity' else 'western'
+        consumer.burn_type = 'activity' if scene == 'activity' else 'natural'
+        outfilename = "{}_out.csv".format(scene)
+        with open(outfilename, 'w') as outfile:
+            run_tests(consumer, fuelbed_list, outfile)
+        reference_values = "{}_Expected.csv".format(scene)
+        ref = compareCSV(reference_values, console=False)
+        computed = compareCSV(outfilename, console=False)
+        (failed, compared) = ref.Compare(computed)
+        print("{}: failed = {} compared = {}".format(outfilename, failed, compared))
+
 
 #-------------------------------------------------------------------------------
 # Start
@@ -93,166 +125,5 @@ SetDefaults(consumer, {})
 fuelbed_list = [str(i[0]) for i in consumer.FCCS.data]
 consumer.fuelbed_fccs_ids = fuelbed_list
 
-scenario_list = ['western', 'southern', 'boreal', 'activity']
-for scene in scenario_list:
-    consumer.fuelbed_ecoregion = scene if scene != 'activity' else 'western'
-    consumer.burn_type = 'activity' if scene == 'activity' else 'natural'
-    outfilename = "{}_out.csv".format(scene)
-    with open(outfilename, 'w') as outfile:
-        run_tests(consumer, fuelbed_list, outfile)
-    reference_values = "{}_Expected.csv".format(scene)
-    ref = compareCSV(reference_values, console=False)
-    computed = compareCSV(outfilename, console=False)
-    (failed, compared) = ref.Compare(computed)
-    print("{}: failed = {} compared = {}".format(outfilename, failed, compared))
-
-
-
-
-
-
-
-##class Driver(object):
-##    """ Drive consume object for testing purposes
-##    """
-##    def __init__(self):
-##        self._consumer = consume.FuelConsumption(
-##            #fccs_file = "input_data/fccs_pyconsume_input.xml")
-##            fccs_file = "input_data/input_without_1000fb.xml")
-##
-##
-##    def _reset_activity_scenario(self):
-##        self._consumer.fuel_moisture_10hr_pct = 15
-##        self._consumer.fuel_moisture_1000hr_pct = 39
-##        self._consumer.fuelbed_area_acres = 10
-##        self._consumer.lengthOfIgnition = 5
-##
-##    def _reset_activity_scenario2(self):
-##        self._consumer.fuel_moisture_10hr_pct = 15
-##        self._consumer.fuel_moisture_1000hr_pct = 45
-##        self._consumer.fuelbed_area_acres = 25
-##
-##    def _reset_activity_scenario3(self):
-##        self._consumer.fuel_moisture_10hr_pct = 17
-##        self._consumer.fuel_moisture_1000hr_pct = 50
-##        self._consumer.fuelbed_area_acres = 100
-##
-##    def _reset_activity_scenario4(self):
-##        self._consumer.fuel_moisture_10hr_pct = 25
-##        self._consumer.fuel_moisture_1000hr_pct = 55
-##        self._consumer.fuelbed_area_acres = 100
-##
-##
-##    def write_header(self, results, catagory_list, stream):
-##        out = "fuelbed"
-##        for i in catagory_list:
-##            ### - this needs to stay in sync with the way the data is printed
-##            sorted_keys = sorted(results[i].keys())
-##            for j in sorted_keys:
-##                out += "," + j
-##        out += '\n'
-##        stream.write(out)
-##
-##    def write_header_alternate(self, catagory_list, stream):
-##        out = "fuelbed"
-##        for i in catagory_list:
-##            out += "," + i
-##        out += '\n'
-##        stream.write(out)
-##
-##    def check_emissions(self):
-##        ''' stub '''
-##        #emissions = consume.Emissions(self._consumer)
-##        #emissions.report(csv="00emiss.csv")
-##        #results = emissions.results()
-##        #print(results)
-##
-##    def write_csv(self, results, fuelbed_list, stream, header, debug):
-##    	### - top-level catagory list
-##        catagory_list = ['summary', 'canopy', 'ground fuels', 'litter-lichen-moss',
-##            'nonwoody', 'shrub', 'woody fuels']
-##        if debug:
-##            catagory_list.append('debug')
-##
-##        if header:
-##            self.write_header(results, catagory_list, stream)
-##        for fb_index in xrange(0, len(fuelbed_list)):
-##            out = fuelbed_list[fb_index]
-##            for cat in catagory_list:
-##                ### - this needs to stay in sync with the way the header is printed
-##                sorted_keys = sorted(results[cat].keys())
-##                for key in sorted_keys:
-##                    out += "," + str(results[cat][key]['total'][fb_index])
-##            out += '\n'
-##            stream.write(out)
-##
-##    def write_csv_alternate(self, results, fuelbed_list, stream, header):
-##        if header:
-##            columns = ['flaming', 'residual', 'smoldering', 'total']
-##            self.write_header_alternate(results, columns, stream)
-##        for fb_index in xrange(0, len(fuelbed_list)):
-##            out = fuelbed_list[fb_index]
-##            sorted_keys = sorted(results['summary']['total'].keys())
-##            for key in sorted_keys:
-##                out += "," + str(results['summary']['total'][key][fb_index])
-##            out += '\n'
-##            stream.write(out)
-##
-##    def write_csv_alt3(self, results, fuelbed_list, stream, header):
-##        # use all the emission keys except 'stratum'
-##        emissions_keys = sorted(results['emissions'].keys())
-##        emissions_keys = [key for key in emissions_keys if key != 'stratum']
-##        cons_keys = sorted(results['consumption']['summary']['total'].keys())
-##
-##        # build up the column headers
-##        columns = []
-##        for key in cons_keys:
-##            columns.append("{}_{}".format("cons", key))
-##        for i in emissions_keys:
-##            for j in cons_keys:
-##                columns.append("{}_{}".format(i, j))
-##
-##        if header:
-##            self.write_header_alternate(columns, stream)
-##        for fb_index in xrange(0, len(fuelbed_list)):
-##            out = fuelbed_list[fb_index]
-##
-##            # print the consumption column values
-##            for key in cons_keys:
-##                out += "," + str(results['consumption']['summary']['total'][key][fb_index])
-##
-##            # print the emission column values
-##            for i in emissions_keys:
-##                for j in cons_keys:
-##                    out += "," + str(results['emissions'][i][j][fb_index])
-##            out += '\n'
-##            stream.write(out)
-##
-
-##def SetDefaults(consumer):
-##    consumer.burn_type = 'natural'
-##    consumer.fuelbed_area_acres = 100
-##    consumer.fuel_moisture_1000hr_pct = 20
-##    consumer.fuel_moisture_duff_pct = 20
-##    consumer.canopy_consumption_pct = 20
-##    consumer.shrub_blackened_pct = 50
-##    consumer.output_units = 'tons_ac'
-##    # - activity specific
-##    consumer.days_since_rain = 20
-##    consumer.fuel_moisture_10hr_pct = 10
-##    consumer.fm_type = 'MEAS-Th'
-##    consumer.lengthOfIgnition = 30
-##    consumer.slope = 5
-##    consumer.windspeed = 5
-
-
-
-
-
-
-
-
-
-
-
-
+run_basic_scenarios(consumer, fuelbed_list)
+run_additional_activity_scenarios(consumer, fuelbed_list)
