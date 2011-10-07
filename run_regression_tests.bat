01ad4692e94ff569481ecb753b358be70a78992d
kjell_swedin <devnull@localhost>
2011-10-07 11:31:38 -0700

    augment regression test batch file

--- run_regression_tests.bat
+++ run_regression_tests.bat
@@ -1,1 +1,7 @@
-@set PYTHONPATH=%PYTHONPATH%;%cd%\consume && c:\progfiles_x86\Python27\python.exe .\test\test_driver.py | findstr failed
+@echo off
+if "%1"=="full" goto FULL
+set PYTHONPATH=%PYTHONPATH%;%cd%\consume && c:\progfiles_x86\Python27\python.exe .\test\test_driver.py | findstr failed
+goto DONE
+:FULL
+set PYTHONPATH=%PYTHONPATH%;%cd%\consume && c:\progfiles_x86\Python27\python.exe .\test\test_driver.py
+:DONE
