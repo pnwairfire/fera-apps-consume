45e6b112683115efb8add67038f631a606e398be
kjells <devnull@localhost>
2012-06-27 16:19:17 -0700

    revise the consume_batch command line processor. Add unit test file.

--- cmdline.py
+++ cmdline.py
@@ -54,92 +54,118 @@ Save the file to a name of your choice to avoid having it overwritten.
         print("\nSuccess: Consume column configuration file written - {}\n".format(filename))
 
 def make_parser():
+    ''' This is the parser for the consume_batch command line
+    '''
+    # - general description of the program and how to use it
     USAGE = r'''
-Generate consumption, emissions, and heat release data.
 
-The Consume batch calculator can be used 2 different ways and with a number of different
-options. In either case, an input file specifying additional parameters is necessary.
-
-1.) Prompt for inputs. This path will interactively prompt for required input data.
-
-2.) Supply all inputs via command line arguments. As mentioned above, the only required argument 
-is an input file in comma-separated (csv) format. The input file can contain 1 of 2 parameter sets
+The Consume batch calculator requires a burn type argument, either 'natural' or 'activity', and
+an input file with fuelbed and environmental data. The input file can contain 1 of 2 parameter sets
 depending on the burn_type specified. See input_natural.csv and input_activity.csv for examples of the
 required input parameters.
 
-Additional options, detailed below, are available when all inputs are specified via command line arguments.
-    '''
+Additional options are detailed below.'''
+
+    # - example of program usage, this is printed after the generated argument descriptions
+    EPILOG = '''
+Examples:
+    // simple case
+    consume_batch.exe natural input_natural.csv
+    
+    // same thing on linux
+    python consume_batch.py natural input_natural.csv
+
+    // specify an alternative loadings file
+    consume_batch.exe natural input_natural.csv -f my_loadings.xml
+
+    // specify a column configuration file
+    consume_batch.exe activity input_activity.csv -x column_config.xml'''
+
+    # - build the parser
     parser = argparse.ArgumentParser(
         description=USAGE,
+        epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    # - required parameter
-    parser.add_argument('-c', action='store', nargs=1, dest='csv_file', required=True,
-        help='REQUIRED - Specify the csv input file. See input_natural.csv and input_activity.csv \
-        for examples of the required parameter sets')
-
-    # - invoke the prompt-for-input path
-    parser.add_argument('-p', action='store_true', dest='do_prompt', default=False,
-        help='Prompt for required inputs (burn_type, input file, 1000hr fuel moisture (activity))')
+    # - add the positional arguments
+    parser.add_argument('burn_type', metavar='burn type\t(activity | natural)', nargs='?')
+    parser.add_argument('csv_file', metavar='input file\t(csv format)', nargs='?')
 
     # - specify an alternative loadings file
-    parser.add_argument('-f', action='store', nargs=1, dest='fuel_loadings_file',
+    parser.add_argument('-f', action='store', nargs=1, dest='fuel_loadings_file', metavar='loadings file',
         help='Specify the fuel loadings file for consume to use. Run the FCCS batch processor over \
             the fuelbeds for which you want to generate consumption/emission results to create\
             a fuel loadings file.')
 
     # - customize the output column configuration
-    parser.add_argument('-x', action='store', nargs=1, dest='col_cfg_file',
+    parser.add_argument('-x', action='store', nargs=1, dest='col_cfg_file', metavar='output columns',
         help='Specify the output column configuration file for consume to use')
 
     # - generate a default column configuration file from which to work
     parser.add_argument('-g', action='store', nargs='?',
-        default="", const="output_config.xml", dest='gen_col_cfg',
+        default="", const="output_config.xml", dest='gen_col_cfg', metavar='col cfg file',
         help='This option will print a default column configuration xml file. The output file is \
             called \"output_config.xml\". Modify this file as needed and save it to another name. \
             Each time the -g option is run the configuration file is overwritten.'
         )
     return parser
+    
+class ConsumeParserException(Exception):
+    pass
 
 class ConsumeParser(object):
+    ''' Parse the consume_batch command line arguments
+    '''
     def __init__(self, argv):
+        self._burn_type = None
         self._csv_file = None
-        self._col_cfg_file = None
         self._fuel_loadings_file = None
+        self._col_cfg_file = None
+        self.do_parse(argv)
+    
+    def do_parse(self, argv):
         parser = make_parser()
         if 1 == len(argv):
             parser.parse_args(['--help'])
         else:
-            args = parser.parse_args(argv[1:])
+            args = parser.parse_args(argv)
 
-            # Generate a column configuration file
+            # Generate a column configuration file and exit
             if args.gen_col_cfg:
                 print_default_column_config_xml(args.gen_col_cfg)
                 sys.exit(0)
 
-            if args.do_prompt:
-                print('\nPrompting for required inputs...')
-                sys.exit(0)
+            # - verify burn_type
+            if not args.burn_type:
+                raise(ConsumeParserException("\nError: A burn_type is required."))
+            if 'natural' != args.burn_type and 'activity' != args.burn_type:
+                raise(ConsumeParserException("\nError: The burn_type must be 'natural' or 'activity'."))
+            self._burn_type = args.burn_type
 
-            # check for valid input file and optional fuel loadings file
-            if args.csv_file:
-                if self.exists(args.csv_file[0]):
-                    self._csv_file = os.path.abspath(args.csv_file[0])
-                if args.fuel_loadings_file:
-                    if self.exists(args.fuel_loadings_file[0]):
-                        self._fuel_loadings_file = os.path.abspath(args.fuel_loadings_file[0])
-            else:
-                print("\nError: an input file is required.\n")
-                sys.exit(1)
-            if args.col_cfg_file and self.exists(args.col_cfg_file[0]):
-                self._col_cfg_file = args.col_cfg_file[0]
+            # check for valid input file
+            if not args.csv_file:
+                raise(ConsumeParserException("\nError: An input file in csv format is required."))
+            if not self.exists(args.csv_file):
+                raise(ConsumeParserException("\nError: The file '{}' does not exist.".format(args.csv_file)))
+            self._csv_file = os.path.abspath(args.csv_file)
+
+            # -  check for optional fuel loadings file
+            if args.fuel_loadings_file:
+                if not self.exists(args.fuel_loadings_file[0]):
+                    raise(ConsumeParserException("\nError: The loadings file '{}' does not exist.".format(args.fuel_loadings_file[0])))
+                self._fuel_loadings_file = os.path.abspath(args.fuel_loadings_file[0])
+
+            if args.col_cfg_file:
+                if not self.exists(args.col_cfg_file[0]):
+                    raise(ConsumeParserException("\nError: The column config file '{}' does not exist.".format(args.col_cfg_file[0])))
+                self._col_cfg_file = os.path.abspath(args.col_cfg_file[0])
+
 
     def exists(self, filename):
-        if not os.path.exists(filename):
-            print("Error: file not found \"{}\"".format(filename))
-            sys.exit(1)
-        return True
+        return True if os.path.exists(filename) else False
 
+    @property
+    def burn_type(self): return self._burn_type
     @property
     def csv_file(self): return self._csv_file
     @property
---
+++ unittest/testCmdline.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+import cmdline as cmd
+
+
+class TestCmdline(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def reset_consumer(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testPassing(self):
+        p = cmd.ConsumeParser(['activity', 'consume_batch.py'])
+        self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
+        self.assertEqual(p.burn_type, 'activity')
+        
+    def testPassingWithLoadingsFile(self):
+        p = cmd.ConsumeParser(['activity', 'consume_batch.py', '-f', 'consume_batch.py'])
+        self.assertEqual(p.burn_type, 'activity')
+        self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
+        self.assertEqual(p.fuel_loadings_file, os.path.abspath('consume_batch.py'))
+        
+    def testPassingWithLoadingsFileAndColCfgFile(self):
+        p = cmd.ConsumeParser(['activity', 'consume_batch.py', '-f', 'consume_batch.py', '-x', 'consume_batch.py'])
+        self.assertEqual(p.burn_type, 'activity')
+        self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
+        self.assertEqual(p.fuel_loadings_file, os.path.abspath('consume_batch.py'))
+        self.assertEqual(p.col_cfg_file, os.path.abspath('consume_batch.py'))
+
+    def testBadBurnType(self):
+        self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('bogus', 'consume_batch.py'))
+
+    def testBadFileType(self):
+        self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'non_existent.py'))
+        
+    def testLoadingsFileDoesntExistType(self):
+        self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'consume_batch.py', '-f', 'non_existent.py'))
+        
+    def testColCfgFileDoesntExistType(self):
+        self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'consume_batch.py', '-x', 'non_existent.py'))
+        
+        
+        
