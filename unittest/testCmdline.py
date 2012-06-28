f135d795e6c18dd47c93d362d95abc4b7371a2f5
kjells <devnull@localhost>
2012-06-28 13:17:57 -0700

    rename some unit tests

--- unittest/testCmdline.py
+++ unittest/testCmdline.py
@@ -15,17 +15,23 @@ class TestCmdline(unittest.TestCase):
         pass
 
     def testPassing(self):
+        ''' Use consume_batch.py as a file that should always be there
+        '''
         p = cmd.ConsumeParser(['app_name_placeholder', 'activity', 'consume_batch.py'])
         self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
         self.assertEqual(p.burn_type, 'activity')
         
     def testPassingWithLoadingsFile(self):
+        ''' Use consume_batch.py as a file that should always be there
+        '''
         p = cmd.ConsumeParser(['app_name_placeholder', 'activity', 'consume_batch.py', '-f', 'consume_batch.py'])
         self.assertEqual(p.burn_type, 'activity')
         self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
         self.assertEqual(p.fuel_loadings_file, os.path.abspath('consume_batch.py'))
         
     def testPassingWithLoadingsFileAndColCfgFile(self):
+        ''' Use consume_batch.py as a file that should always be there
+        '''
         p = cmd.ConsumeParser(['app_name_placeholder', 'activity', 'consume_batch.py', '-f', 'consume_batch.py', '-x', 'consume_batch.py'])
         self.assertEqual(p.burn_type, 'activity')
         self.assertEqual(p.csv_file, os.path.abspath('consume_batch.py'))
@@ -35,13 +41,13 @@ class TestCmdline(unittest.TestCase):
     def testBadBurnType(self):
         self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('bogus', 'consume_batch.py'))
 
-    def testBadFileType(self):
+    def testNonExistentFile(self):
         self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'non_existent.py'))
         
-    def testLoadingsFileDoesntExistType(self):
+    def testLoadingsFileDoesntExist(self):
         self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'consume_batch.py', '-f', 'non_existent.py'))
         
-    def testColCfgFileDoesntExistType(self):
+    def testColCfgFileDoesntExist(self):
         self.assertRaises(cmd.ConsumeParserException, cmd.ConsumeParser, ('activity', 'consume_batch.py', '-x', 'non_existent.py'))
         
         
