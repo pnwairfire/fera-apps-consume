f06d111a4ef59eb4f2a39fae3ad963b917122e5e
kjells <devnull@localhost>
2013-03-06 16:20:23 -0800

    add new column output strategy, flatten the results dictionary, pick the columns to print

--- consume_batch.py
+++ consume_batch.py
@@ -13,6 +13,40 @@ import sys
 from custom_col import CustomCol
 import batch_locator
 import cmdline
+import pandas as pan
+
+
+# -- From stackoverflow.com ---
+from collections import *
+from itertools import *
+
+same = lambda x:x  # identity function
+add = lambda a,b:a+b
+_tuple = lambda x:(x,)  # python actually has coercion, avoid it like so
+
+def flattenDict(dictionary, keyReducer=add, keyLift=_tuple, init=()):
+
+    # semi-lazy: goes through all dicts but lazy over all keys
+    # reduction is done in a fold-left manner, i.e. final key will be
+    #     r((...r((r((r((init,k1)),k2)),k3))...kn))
+
+    def _flattenIter(pairs, _keyAccum=init):
+        atoms = ((k,v) for k,v in pairs if not isinstance(v, Mapping))
+        submaps = ((k,v) for k,v in pairs if isinstance(v, Mapping))
+        def compress(k):
+            return keyReducer(_keyAccum, keyLift(k))
+        return chain(
+            (
+                (compress(k),v) for k,v in atoms
+            ),
+            *[
+                _flattenIter(submap.items(), compress(k))
+                for k,submap in submaps
+            ]
+        )
+    return dict(_flattenIter(dictionary.items()))
+# -- end from stackoverflow.com ---
+
 
 RESULTS_FILE = 'batch_results.csv'
 
@@ -219,6 +253,48 @@ def write_results(all_results, cols, fuelbed_list):
         write_header(rename_columns(columns), outfile)
         write_computed_results(all_results, columns, fuelbed_list, outfile)
 
+def write_results2(all_results, col_cfg_file=None):
+    default_cols = [
+        ('parameters_fuelbeds', 'Fuelbeds'),
+        ('consumption_summary_total_total', 'Total Consumption'),
+        ('consumption_summary_canopy_total', 'Canopy Consumption'),
+        ('consumption_summary_ground_fuels_total', 'GroundFuel Consumption'),
+        ('consumption_summary_litter-lichen-moss_total', 'LLM Consumption'),
+        ('consumption_summary_nonwoody_total', 'NonWoody Consumption'),
+        ('consumption_summary_shrub_total', 'Shrub Consumption'),
+        ('consumption_summary_woody_fuels_total', 'Woody Consumption'),
+        ('emissions_ch4_total', 'CH4 Emissions'),
+        ('emissions_co2_total', 'CO2 Emissions'),
+        ('emissions_co_total', 'CO Emissions'),
+        ('emissions_nmhc_total', 'NMHC Emissions'),
+        ('emissions_pm10_total', 'PM10 Emissions'),
+        ('emissions_pm25_total', 'PM25 Emissions'),
+        ('emissions_pm_total', 'PM Emissions'),
+        ('heat_release_total', 'Total Heat Release'),
+        ('parameters_area', 'Area'),
+        ('parameters_burn_type', 'Burn Type'),
+        ('parameters_can_con_pct', 'Canopy Consumption (%)'),
+        ('parameters_ecoregion', 'Region'),
+        ('parameters_emissions_fac_group', 'Emmissions Factor Group'),
+        ('parameters_fm_1000hr', '1000hr Fuel Moisture'),
+        ('parameters_fm_duff', 'Duff Fuel Moisture'),
+        ('parameters_shrub_black_pct', 'Shrub Blackened (%)'),
+        ('parameters_units', 'Units')
+    ]
+    tmp = {}
+    for k,v in flattenDict(all_results).iteritems():
+        colname = '_'.join(k)
+        colname = colname.replace(' ', '_')
+        tmp[colname] = v
+    add_these = []
+    for col in default_cols:
+        key = col[0]
+        new_key = col[1]
+        if tmp.has_key(key):
+            add_these.append((new_key, tmp[key]))
+    newdf = pan.DataFrame.from_items(add_these)
+    newdf.to_csv(RESULTS_FILE, index=False)
+
 
 def run(burn_type, csv_input, msg_level, fuel_loadings=None, col_cfg=None):
     #consumer = consume.FuelConsumption(fccs_file=get_input_file(fuel_loadings), msg_level=msg_level) \
@@ -230,7 +306,8 @@ def run(burn_type, csv_input, msg_level, fuel_loadings=None, col_cfg=None):
         results = emissions.results()
         cols = CustomCol() if not col_cfg else CustomCol.from_file(col_cfg)
         fuelbed_list = consumer.fuelbed_fccs_ids
-        write_results(results, cols, fuelbed_list)
+        #write_results(results, cols, fuelbed_list)
+        write_results2(results)
         print("\nSuccess!!! Results are in \"{}\"".format(RESULTS_FILE))
 
 #-------------------------------------------------------------------------------
