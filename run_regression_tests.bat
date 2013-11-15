883902a783de8c3e43637c5fb0af5d1a71932322
kjells <devnull@localhost>
2013-11-15 09:51:17 -0800

    show exceptions from batch file

--- run_regression_tests.bat
+++ run_regression_tests.bat
@@ -2,7 +2,8 @@
 if "%1"=="full" goto FULL
 python .\test\test_driver.py > 00results.txt
 type 00results.txt | findstr Exception
-type 00results.txt | findstr failedgoto DONE
+type 00results.txt | findstr failed
+goto DONE
 :FULL
 
 python .\test\test_driver.py
