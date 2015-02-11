e8e38961efa81445403df8b5e3af43bec652fc2c
Joel Dubowy <jdubowy@gmail.com>
2015-02-11 11:46:22 -0800

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+# Note: using pip.req.parse_requirements like so:
+#  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+# can result in the folloing error (for example, on Heroku):
+#    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+
+setup(
+    name='consume',
+    version='4.1',
+    author='Michigan Tech Research, Fire and Environmental Research Applications Team (FERA)',
+    #author_email='',
+    packages=[
+        'consume'
+    ],
+    package_data={
+        'consume': ['input_data/*.xml']
+    },
+    scripts=[],
+    url='ssh://hg@bitbucket.org/fera/apps-consume4',
+    description='Calculates estimated fuel consumption by wild fires.',
+    install_requires=REQUIREMENTS,
+)
