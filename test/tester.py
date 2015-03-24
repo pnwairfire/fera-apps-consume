275f1f72120acc0f9d41b0d3f9d46f638fd074a3
kjells <devnull@localhost>
2015-03-24 11:45:12 -0700

    tweak import stuff some more

--- test/tester.py
+++ test/tester.py
@@ -8,9 +8,11 @@
 # Copyright:   (c) kjells 2011
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
+from __future__ import absolute_import
 import csv
 import sys
 from . ulp import WithinThisManyULP, HowManyULP
+#from ulp import WithinThisManyULP, HowManyULP
 import decimal as dec
 import re
 import math
