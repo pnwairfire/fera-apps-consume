f24176077687b69dba6e78fab5b5bce62a329ae1
sprich <devnull@localhost>
2015-05-04 06:52:34 -0700

    updated all tests

--- run_regression_tests.bat
+++ run_regression_tests.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%1"=="full" goto FULL
-python -m test.test_driver > 00results.txt
+c:\FuelFireTools_Apr27\bin\python -m test.test_driver > 00results.txt
 type 00results.txt | findstr Exception
 type 00results.txt | findstr failed
 rem python consume_batch.py natural test\1_458_english.csv
@@ -9,6 +9,5 @@ rem python consume_batch.py natural test\1_458_metric.csv
 rem diff consume_results.csv test\expected\1_458_metric_out.csv
 goto DONE
 :FULL
-
-python -m test.test_driver 
+c:\FuelFireTools_Apr27\bin\python -m test.test_driver 
 :DONE
