198f9b7699c55755f91f3823bf84893eb5536949
kjells <kjells@uw.edu>
2015-07-30 14:18:50 -0700

    revisit consume versioning strategy - add version.py file

---
+++ consume/version.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        version.py
+# Purpose:     Simply provides versioning information for consume and
+#               consume_batch.
+#              The CI server appends a line to this file so that a deployed
+#               version looks like:
+#
+#              MAJOR_VERSION = 4
+#              MINOR_VERSION = 2
+#              BUILD_REVISION = ###
+#
+# Author:      kjells
+#
+# Created:     7/30/2015
+#-------------------------------------------------------------------------------
+MAJOR_VERSION = 4
+MINOR_VERSION = 2
+
