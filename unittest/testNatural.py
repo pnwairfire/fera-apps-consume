954e35412b2bd715966a24ecbc0ce84ddd8b7eeb
kjells <kjell.swedin@gmail.com>
2016-10-18 13:24:34 -0700

    conditionalize unit test output for cleaner look. Use 'green' as test runner. Test upper and lower duff consumptions

--- unittest/testNatural.py
+++ unittest/testNatural.py
@@ -25,9 +25,10 @@ def bracket(load, cons):
     # ensure that results are between 0 and initial load value)
     return np.where(0 > cons, 0, np.where(cons > load, load, cons))
     
-def print_test_name(name):
-    print('\n^^^^^^^^^^^^^^^^^^^\n{}'.format(name))
-
+def my_print(stuff):
+    pass
+    #print(stuff)
+    
 class TestNaturalEquations(unittest.TestCase):
 
     def setUp(self):
@@ -35,7 +36,7 @@ class TestNaturalEquations(unittest.TestCase):
         self.fc = consume.FuelConsumption(fccs_file=loadings_file)
         self.fc.burn_type = 'natural'
         input_file = helper.get_test_inputfile()
-        # debug: print(' - Loading input from: {}'.format(input_file))
+        # debug: my_print(' - Loading input from: {}'.format(input_file))
         self.fc.load_scenario(load_file=input_file)
         self._loadings = self.fc._get_loadings_for_specified_files(self.fc._settings.get('fuelbeds'))
 
@@ -44,8 +45,8 @@ class TestNaturalEquations(unittest.TestCase):
                    "masks": {"boreal": 0, "western": 0, "southern": 1},
                    "maskw": {"boreal": 0, "western": 1, "southern": 0}}
 
-        print('---')
-        print('\t'.join(self.fc._settings.get('ecoregion')))
+        my_print('--- ecoregion settings')
+        my_print('\t'.join(self.fc._settings.get('ecoregion')))
 
         ecoregion = self.fc._settings.get('ecoregion')
         self._ecob_mask = [self._ecodict["maskb"][e] for e in ecoregion]
@@ -60,17 +61,16 @@ class TestNaturalEquations(unittest.TestCase):
         pass
 
     def extract_shrub_herb_totals(self, ret):
-        print('\nType: {}'.format(type(ret)))
+        my_print('\nType: {}'.format(type(ret)))
         totals = np.zeros_like(ret[0][:, ][3])
         for i, v in enumerate(ret):
             totals += v[:, ][3]
         return totals
         
     def test_herb_calc(self): 
-        print_test_name('test_herb_calc')
         ret = ccn.herb_calc(self._loadings, self._ecoregion_masks)
         totals = self.extract_shrub_herb_totals(ret)
-        print(totals)
+        my_print(totals)
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(0.93, totals[0], places=2)    # western
         self.assertAlmostEqual(2.91, totals[1], places=2)    # southern
@@ -92,10 +92,9 @@ class TestNaturalEquations(unittest.TestCase):
             tmp = -0.1889 + 0.9049*log_loading + 0.0676*season
             return to_tons(np.e**tmp)
 
-        print_test_name('test_shrub_calc')
         ret = ccn.shrub_calc(self.fc.shrub_blackened_pct, self._loadings, self._ecoregion_masks, 0)
         totals = self.extract_shrub_herb_totals(ret)
-        print(totals)
+        my_print(totals)
 
         self.assertAlmostEqual(1, totals[0], places=2)
         self.assertAlmostEqual(southern(3, 0), totals[1], places=4)
@@ -109,8 +108,7 @@ class TestNaturalEquations(unittest.TestCase):
 
     def test_sound_one_calc(self): 
         ret = ccn.sound_one_calc(self._loadings, self._ecos_mask)
-        print_test_name('test_sound_one_calc')
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         totals = ret[3]
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(0.4235, totals[0], places=2)
@@ -124,9 +122,8 @@ class TestNaturalEquations(unittest.TestCase):
         self.assertAlmostEqual(0.0, totals[8], places=2)
 
     def test_sound_ten_calc(self): 
-        print_test_name('test_sound_ten_calc')  # print totals
         ret = ccn.sound_ten_calc(self._loadings, self._ecos_mask)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         totals = ret[3]
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(0.42, totals[0], places=2)
@@ -140,9 +137,8 @@ class TestNaturalEquations(unittest.TestCase):
         self.assertAlmostEqual(0.0, totals[8], places=2)
 
     def test_sound_hundred_calc(self): 
-        print_test_name('test_sound_hundred_calc')  # print totals
         ret = ccn.sound_hundred_calc(self._loadings, self._ecos_mask)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         totals = ret[3]
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(0.36, totals[0], places=2)
@@ -157,7 +153,6 @@ class TestNaturalEquations(unittest.TestCase):
 
     def test_sound_large_wood_calc(self):  
         # test loading amounts are (1,2,1) = 4, (3,6,3) = 12, (5,10,5) = 20
-        print_test_name('test_sound_large_wood')
         TEST_FM = 50
         fm_from_file = self.fc.fuel_moisture_1000hr_pct
         self.fc.fuel_moisture_1000hr_pct = TEST_FM
@@ -165,7 +160,7 @@ class TestNaturalEquations(unittest.TestCase):
         
         one_k, ten_k, tenk_plus = ccn.sound_large_wood_calc(self._loadings, self.fc.fuel_moisture_1000hr_pct)
         totals = one_k[3] + ten_k[3] + tenk_plus[3]
-        print(totals)
+        my_print(totals)
         
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(eq(4, self.fc.fuel_moisture_1000hr_pct), totals[0], places=4)
@@ -215,7 +210,6 @@ class TestNaturalEquations(unittest.TestCase):
         
 
     def test_rotten_large_wood_calc(self):  
-        print_test_name('test_rotten_large_wood')
         # test loading amounts are (1,2,1) = 4, (3,6,3) = 12, (5,10,5) = 20
         TEST_FM = 50
         fm_from_file = self.fc.fuel_moisture_1000hr_pct
@@ -227,7 +221,7 @@ class TestNaturalEquations(unittest.TestCase):
             
         one_k, ten_k, tenk_plus = ccn.rotten_large_wood_calc(self._loadings, self.fc.fuel_moisture_1000hr_pct)
         totals = one_k[3] + ten_k[3] + tenk_plus[3]
-        print(totals)  # print totals
+        my_print(totals)  # print totals
         self.assertEqual(9, len(totals))
         self.assertAlmostEqual(calc(4, self.fc.fuel_moisture_1000hr_pct), totals[0], places=4)
         self.assertAlmostEqual(calc(12, self.fc.fuel_moisture_1000hr_pct), totals[1], places=4)
@@ -278,17 +272,16 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 1,3, and 5 for these catagories
         #
-        print_test_name('test_litter_calc')
         FM_DUFF = 30
-        print(self._ecoregion_masks)
-        print(self._loadings['litter_loading'])
+        my_print(self._ecoregion_masks)
+        my_print(self._loadings['litter_loading'])
         
         fm_from_file = self.fc.fuel_moisture_duff_pct
         self.fc.fuel_moisture_duff_pct = FM_DUFF
         
         ret = ccn.litter_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_1000hr_pct, self._ecoregion_masks)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         
         total = ret[3]
         self.assertEqual(9, len(total))
@@ -308,17 +301,16 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 1,3, and 5 for these catagories
         #
-        print_test_name('test_lichen_calc')
         FM_DUFF = 30
-        print(self._ecoregion_masks)
-        print(self._loadings['lichen_loading'])
+        my_print(self._ecoregion_masks)
+        my_print(self._loadings['lichen_loading'])
         
         fm_from_file = self.fc.fuel_moisture_duff_pct
         self.fc.fuel_moisture_duff_pct = FM_DUFF
         
         ret = ccn.lichen_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_1000hr_pct, self._ecoregion_masks)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         
         total = ret[3]
         self.assertEqual(9, len(total))
@@ -338,17 +330,16 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 1,3, and 5 for these catagories
         #
-        print_test_name('test_moss_calc')
         FM_DUFF = 30
-        print(self._ecoregion_masks)
-        print(self._loadings['moss_loading'])
+        my_print(self._ecoregion_masks)
+        my_print(self._loadings['moss_loading'])
         
         fm_from_file = self.fc.fuel_moisture_duff_pct
         self.fc.fuel_moisture_duff_pct = FM_DUFF
         
         ret = ccn.moss_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_1000hr_pct, self._ecoregion_masks)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         
         total = ret[3]
         self.assertEqual(9, len(total))
@@ -368,7 +359,6 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 0.5, 10, and 150 (for each lower, upper duff so 1, 20, 300 for total duff load)
         #
-        print_test_name('test_duff_calc')
         FM_DUFF = 30
         print(self._ecoregion_masks)
         print(self._loadings['duff_upper_loading'])
@@ -380,7 +370,7 @@ class TestNaturalEquations(unittest.TestCase):
         cons_duff_upper, cons_duff_lower = ccn.duff_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_litter_pct, self._ecoregion_masks)
         total = cons_duff_upper[3] + cons_duff_lower[3]
-        print(total)  # print totals
+        my_print(total)  # print totals
         self.assertEqual(9, len(total))
         self.assertAlmostEqual(0, total[0], places=2)
         self.assertAlmostEqual(1.96, total[1], places=2)
@@ -392,16 +382,15 @@ class TestNaturalEquations(unittest.TestCase):
         self.assertAlmostEqual(0.0, total[7], places=2)
         self.assertAlmostEqual(0.0, total[8], places=2)
         
-        '''
         # waiting for spreadsheet update
         # upper duff
         total = cons_duff_upper[3]
         self.assertAlmostEqual(0, total[0], places=2)
         self.assertAlmostEqual(1.96, total[1], places=2)
-        self.assertAlmostEqual(11.62, total[2], places=2)
-        self.assertAlmostEqual(192.38, total[3], places=2)
-        self.assertAlmostEqual(0.63, total[4], places=2)
-        self.assertAlmostEqual(192.38, total[5], places=2)
+        self.assertAlmostEqual(10.0, total[2], places=2)
+        self.assertAlmostEqual(150.0, total[3], places=2)
+        self.assertAlmostEqual(0.5, total[4], places=2)
+        self.assertAlmostEqual(150.0, total[5], places=2)
         self.assertAlmostEqual(0.0, total[6], places=2)
         self.assertAlmostEqual(0.0, total[7], places=2)
         self.assertAlmostEqual(0.0, total[8], places=2)
@@ -409,15 +398,14 @@ class TestNaturalEquations(unittest.TestCase):
         # lower duff
         total = cons_duff_lower[3]
         self.assertAlmostEqual(0, total[0], places=2)
-        self.assertAlmostEqual(1.96, total[1], places=2)
-        self.assertAlmostEqual(11.62, total[2], places=2)
-        self.assertAlmostEqual(192.38, total[3], places=2)
-        self.assertAlmostEqual(0.63, total[4], places=2)
-        self.assertAlmostEqual(192.38, total[5], places=2)
+        self.assertAlmostEqual(0, total[1], places=2)
+        self.assertAlmostEqual(1.62, total[2], places=2)
+        self.assertAlmostEqual(42.38, total[3], places=2)
+        self.assertAlmostEqual(0.13, total[4], places=2)
+        self.assertAlmostEqual(42.38, total[5], places=2)
         self.assertAlmostEqual(0.0, total[6], places=2)
         self.assertAlmostEqual(0.0, total[7], places=2)
         self.assertAlmostEqual(0.0, total[8], places=2)
-        '''
 
         self.fc.fuel_moisture_duff_pct = fm_from_file
 
@@ -426,11 +414,10 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 1, 5, 10 for these catagories
         #
-        print_test_name('test_basal_acc_calc')
         FM_DUFF = 30
         FM_LITTER = 10
-        print(self._ecoregion_masks)
-        print(self._loadings['bas_loading'])
+        my_print(self._ecoregion_masks)
+        my_print(self._loadings['bas_loading'])
         
         fm_duff_from_file = self.fc.fuel_moisture_duff_pct
         fm_litter_from_file = self.fc.fuel_moisture_litter_pct
@@ -439,7 +426,7 @@ class TestNaturalEquations(unittest.TestCase):
         
         ret = ccn.basal_accumulation_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_litter_pct, self._ecoregion_masks)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         
         total = ret[3]
         self.assertEqual(9, len(total))
@@ -460,11 +447,11 @@ class TestNaturalEquations(unittest.TestCase):
         #
         # NOTE: loadings are 1, 5, 10 for these catagories
         #
-        print_test_name('test_sq_midden_calc')
+        FM_DUFF = 30
         FM_DUFF = 30
         FM_LITTER = 10
-        print(self._ecoregion_masks)
-        print(self._loadings['sqm_loading'])
+        my_print(self._ecoregion_masks)
+        my_print(self._loadings['sqm_loading'])
         
         fm_duff_from_file = self.fc.fuel_moisture_duff_pct
         fm_litter_from_file = self.fc.fuel_moisture_litter_pct
@@ -473,7 +460,7 @@ class TestNaturalEquations(unittest.TestCase):
         
         ret = ccn.basal_accumulation_calc(self._loadings,
                 self.fc.fuel_moisture_duff_pct, self.fc.fuel_moisture_litter_pct, self._ecoregion_masks)
-        print(ret[3])  # print totals
+        my_print(ret[3])  # print totals
         
         total = ret[3]
         self.assertEqual(9, len(total))
