9e88b41afe91e686a8939d136deb2dcfdb2b26f0
Brian Drye <bridrye@gmail.com>
2018-07-09 17:06:53 -0700

    update Visualizer to compare Consume V4 vs V5 output.

--- visualizer/visualizerConsumeOutput276v284.R
+++ visualizer/visualizerConsumeOutput276v284.R
@@ -1,20 +1,38 @@
 #
 # Load this script in RStudio to compare disturbance timestep trends by fuelbed and column
 #
-# The output files that are compared are created using commands like: 
+# The output files are created using Consume version 4 (from FFT) and Consume version 5 (from bitbucket)
+#
+# The input files were created in Landfire (using FCCS build 284)
+# 
+# change directory to Consume v4  (paths below are from Brian Drye's dev machine)
+#
+# python3 consume_batch.py -f ~/repos/uw/landfiredisturbance/run_landfire/deliverables284/consume_loadings.csv 
+# -o consumeV4OutputFromDeliverables284.csv natural ~/repos/uw/apps-consume/visualizer/deliverableInputFileWestern.csv
+
+# python3 consume_batch.py -f ~/repos/uw/landfiredisturbance/run_landfire/baseline284/consume_loadings.csv 
+# -o consumeV4OutputFromBaseline284.csv natural ~/repos/uw/apps-consume/test/regression_input_western.csv
+
+#
+# change directory to Consume v5  
+#
 # python3 consume_batch.py -f ../landfiredisturbance/run_landfire/deliverables284/consume_loadings.csv 
-# -o consumeOutputFromDeliverables284.csv natural 
+# -o consumeV5OutputFromDeliverables284.csv natural 
 # ./visualizer/deliverableInputFileWestern.csv
 #
+# python3 consume_batch.py -f ../landfiredisturbance/run_landfire/baseline284/consume_loadings.csv 
+# -o consumeV5OutputFromBaseline284.csv natural ./test/regression_input_western.csv
+
+
 # (note: the input file deliverableInputFileWestern.csv is a custom input file made to match the number
 #  of rows in the deliverables284/consume_loadings.csv)
 #
 #
 # Copy the following files to the same directory as this script:
-# consumeOutputFromBaseline276.csv
-# consumeOutputFromDeliverables276.csv
-# consumeOutputFromBaseline284.csv
-# consumeOutputFromDeliverables284.csv
+# consumeV4OutputFromBaseline284.csv
+# consumeV4OutputFromDeliverables284.csv
+# consumeV5OutputFromBaseline284.csv
+# consumeV5OutputFromDeliverables284.csv
 #
 # set the working directory to the source file location
 # From RStudio menu: Session->Set Working Directory->To Source File Location
@@ -27,70 +45,70 @@ library(ggplot2)
 library(manipulate)
 library(gridExtra)
 
-fccsBaselineData284 = read.csv("consumeOutputFromBaseline284.csv", header=T)
-dim(fccsBaselineData284)
+v5BaselineData = read.csv("consumeV5OutputFromBaseline284.csv", header=T)
+dim(v5BaselineData)
 
-fccsData284 = read.csv("consumeOutputFromDeliverables284.csv", header=T)
-dim(fccsData284)
+v5DeliverableData = read.csv("consumeV5OutputFromDeliverables284.csv", header=T)
+dim(v5DeliverableData)
 
-fccsBaselineData276 = read.csv("consumeOutputFromBaseline276.csv", header=T)
-dim(fccsBaselineData276)
+v4BaselineData = read.csv("consumeV4OutputFromBaseline284.csv", header=T)
+dim(v4BaselineData)
 
-fccsData276 = read.csv("consumeOutputFromDeliverables276.csv", header=T)
-dim(fccsData276)
+v4DeliverableData = read.csv("consumeV4OutputFromDeliverables284.csv", header=T)
+dim(v4DeliverableData)
 
 
 myPlot <- function(fb_number, disturbance, columnName){
   
   disturbance <- substring(disturbance, 1, 1)
   
-  baselineData276 <- fccsBaselineData276[fccsBaselineData276$fuelbeds == fb_number, c('fuelbeds', columnName, 'filename')]
-  baselineData284 <- fccsBaselineData284[fccsBaselineData284$fuelbeds == fb_number, c('fuelbeds', columnName, 'filename')]
+  v4SelectedBaselineData <- v4BaselineData[v4BaselineData$fuelbeds == fb_number, c('fuelbeds', columnName, 'filename')]
+  v5SelectedBaselineData <- v5BaselineData[v5BaselineData$fuelbeds == fb_number, c('fuelbeds', columnName, 'filename')]
   
   bdtest <- paste(fb_number, disturbance, sep="_")
   bdtest <- apply(expand.grid(bdtest, c(1,2,3)), 1, paste, collapse="")
   bdtest <- apply(expand.grid(bdtest, c(1,2,3)), 1, paste, collapse="")
   bdtest <- sort(bdtest)
   
-  fb_data276 <- fccsData276[fccsData276$fuelbeds %in% bdtest, c('fuelbeds', columnName, 'filename')]
-  prefix276 <- substr(fb_data276$fuelbeds, 0, nchar(fb_number) + 3)
-  timeStep276 <- substr(fb_data276$fuelbeds, nchar(fb_number) + 4, nchar(fb_number) + 4)
+  v4SelectedDeliverableData <- v4DeliverableData[v4DeliverableData$fuelbeds %in% bdtest, c('fuelbeds', columnName, 'filename')]
+  prefixV4 <- substr(v4SelectedDeliverableData$fuelbeds, 0, nchar(fb_number) + 3)
+  timeStepV4 <- substr(v4SelectedDeliverableData$fuelbeds, nchar(fb_number) + 4, nchar(fb_number) + 4)
   
-  fb_data284 <- fccsData284[fccsData284$fuelbeds %in% bdtest, c('fuelbeds', columnName, 'filename')]
-  prefix284 <- substr(fb_data284$fuelbeds, 0, nchar(fb_number) + 3)
-  timeStep284 <- substr(fb_data284$fuelbeds, nchar(fb_number) + 4, nchar(fb_number) + 4)
+  v5SelectedDeliverableData <- v5DeliverableData[v5DeliverableData$fuelbeds %in% bdtest, c('fuelbeds', columnName, 'filename')]
+  prefixV5 <- substr(v5SelectedDeliverableData$fuelbeds, 0, nchar(fb_number) + 3)
+  timeStepV5 <- substr(v5SelectedDeliverableData$fuelbeds, nchar(fb_number) + 4, nchar(fb_number) + 4)
   
-  fb_data276$Prefix <- prefix276
-  fb_data276$TimeStep <- as.numeric(timeStep276)
+  v4SelectedDeliverableData$Prefix <- prefixV4
+  v4SelectedDeliverableData$TimeStep <- as.numeric(timeStepV4)
   
-  fb_data284$Prefix <- prefix284
-  fb_data284$TimeStep <- as.numeric(timeStep284)
+  v5SelectedDeliverableData$Prefix <- prefixV5
+  v5SelectedDeliverableData$TimeStep <- as.numeric(timeStepV5)
   
-  baselineDataExpanded276 <- baselineData276[rep(row.names(baselineData276), length(unique(fb_data276$Prefix))), 1:3]
-  baselineDataExpanded276$Prefix <- unique(fb_data276$Prefix)
-  baselineDataExpanded276$TimeStep <- rep(0, length(unique(fb_data276$Prefix)))
+  v4BaselineDataExpanded <- v4SelectedBaselineData[rep(row.names(v4SelectedBaselineData), length(unique(v4SelectedDeliverableData$Prefix))), 1:3]
+  v4BaselineDataExpanded$Prefix <- unique(v4SelectedDeliverableData$Prefix)
+  v4BaselineDataExpanded$TimeStep <- rep(0, length(unique(v4SelectedDeliverableData$Prefix)))
   
-  baselineDataExpanded284 <- baselineData284[rep(row.names(baselineData284), length(unique(fb_data284$Prefix))), 1:3]
-  baselineDataExpanded284$Prefix <- unique(fb_data284$Prefix)
-  baselineDataExpanded284$TimeStep <- rep(0, length(unique(fb_data284$Prefix)))
+  v5BaselineDataExpanded <- v5SelectedBaselineData[rep(row.names(v5SelectedBaselineData), length(unique(v5SelectedDeliverableData$Prefix))), 1:3]
+  v5BaselineDataExpanded$Prefix <- unique(v5SelectedDeliverableData$Prefix)
+  v5BaselineDataExpanded$TimeStep <- rep(0, length(unique(v5SelectedDeliverableData$Prefix)))
   
-  combinedData276 <- rbind(fb_data276, baselineDataExpanded276)
-  combinedData284 <- rbind(fb_data284, baselineDataExpanded284)
+  v4CombinedData <- rbind(v4SelectedDeliverableData, v4BaselineDataExpanded)
+  v5CombinedData <- rbind(v5SelectedDeliverableData, v5BaselineDataExpanded)
   
-  mygraph276 <- ggplot(data = combinedData276, aes_string(x="TimeStep", y=columnName)) + geom_line(aes(colour=Prefix))
-  mygraph276 <- mygraph276 + guides(colour=guide_legend(title="Disturbance"))
-  mygraph276 <- mygraph276 + ggtitle(paste(combinedData276$filename[1], "Original Calculations (FCCS build 276)", sep=" ")) + xlim(0,3)
-  mygraph276 <- mygraph276 + scale_color_manual(values = c("green4", "orange", "red"))
+  graphV4 <- ggplot(data = v4CombinedData, aes_string(x="TimeStep", y=columnName)) + geom_line(aes(colour=Prefix))
+  graphV4 <- graphV4 + guides(colour=guide_legend(title="Disturbance"))
+  graphV4 <- graphV4 + ggtitle(paste(v4CombinedData$filename[1], "Consume V4", sep=" ")) + xlim(0,3)
+  graphV4 <- graphV4 + scale_color_manual(values = c("green4", "orange", "red"))
   
-  mygraph284 <- ggplot(data = combinedData284, aes_string(x="TimeStep", y=columnName)) + geom_line(aes(colour=Prefix))
-  mygraph284 <- mygraph284 + guides(colour=guide_legend(title="Disturbance"))
-  mygraph284 <- mygraph284 + ggtitle(paste(combinedData284$filename[1], "New Calculations (FCCS build 284)", sep=" ")) + xlim(0,3)
-  mygraph284 <- mygraph284 + scale_color_manual(values = c("green4", "orange", "red"))
+  graphV5 <- ggplot(data = v5CombinedData, aes_string(x="TimeStep", y=columnName)) + geom_line(aes(colour=Prefix))
+  graphV5 <- graphV5 + guides(colour=guide_legend(title="Disturbance"))
+  graphV5 <- graphV5 + ggtitle(paste(v5CombinedData$filename[1], "Consume V5", sep=" ")) + xlim(0,3)
+  graphV5 <- graphV5 + scale_color_manual(values = c("green4", "orange", "red"))
   
-  grid.arrange(mygraph276, mygraph284, nrow=2)
+  grid.arrange(graphV4, graphV5, nrow=2)
   
 }
-manipulate(myPlot(fb_number, disturbance, columnName), fb_number = picker(lapply(fccsBaselineData284$fuelbeds, as.character)), 
+manipulate(myPlot(fb_number, disturbance, columnName), fb_number = picker(lapply(v5BaselineData$fuelbeds, as.character)), 
            disturbance = picker("1 Fire", "2 Mechanical Add", "3 Mechanical Remove", "4 Wind", "5 Insect & Disease"), 
-           columnName = picker(lapply(colnames(fccsBaselineData284), as.character)))
+           columnName = picker(lapply(colnames(v5BaselineData), as.character)))
 
