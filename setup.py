504a20dd786d64ccaa4c3dbda8da01c7542007d3
Joel Dubowy <jdubowy@gmail.com>
2019-03-20 14:46:58 -0700

    Parse version from consume/version.py rather than import consume.version
    import fails if consume is being installed on a system that doesn't yet
    have numpy, pandas, or other dependencies installed.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
+import os
 from setuptools import setup
-from consume import version
 
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
@@ -8,10 +8,26 @@ from consume import version
 with open('requirements.txt') as f:
     REQUIREMENTS = f.read().splitlines()
 
+# HACK: we can't import consume.version because there's no guarantee that
+#   any of the dependencies indirectly imported by consume.__init__.py
+#   (numpy, pandas, etc.) have yet been installed.  So, parse the file
+#   for the version numbers.
+version = {}
+with open('consume/version.py') as f:
+    for line in f.readlines():
+        line = line.strip()
+        if not line.startswith('#'):
+            parts = [p.strip() for p in line.split('=')]
+            if len(parts) == 2:
+                version[parts[0]] = parts[1]
+
 setup(
     name='apps-consume',
-    version='{}.{}.{}'.format(version.MAJOR_VERSION, version.MINOR_VERSION,
-        version.PYPI_BUILD_REVISION),
+    version='{}.{}.{}'.format(
+        version['MAJOR_VERSION'],
+        version['MINOR_VERSION'],
+        version['PYPI_BUILD_REVISION']
+    ),
     author='Fire and Environmental Research Applications Team (FERA)',
     #author_email='',
     packages=[
